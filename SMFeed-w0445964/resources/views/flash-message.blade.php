9d5b2800de33d0f5e254b911dd1fe7b06f3bef30
Taehong <minth1123@icloud.com>
2021-12-06 18:18:08 -0400

    Phase 2 done

--- SMFeed-w0445964/resources/views/flash-message.blade.php
+++ SMFeed-w0445964/resources/views/flash-message.blade.php
@@ -1,3 +1,4 @@
+{{-- Source Code: https://www.itsolutionstuff.com/post/laravel-6-flash-message-tutorialexample.html--}}
 @if ($message = Session::get('error'))
     <div class="alert alert-danger alert-block">
         <button type="button" class="close" data-dismiss="alert">×</button>
