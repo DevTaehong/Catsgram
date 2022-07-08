449e2a37717d87e55639d36f209a8f66ae6d3ee5
Taehong <minth1123@icloud.com>
2022-07-08 16:21:34 -0300

    Changing minor issues

--- QutesApp/resources/views/flash-message.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- Source Code: https://www.itsolutionstuff.com/post/laravel-6-flash-message-tutorialexample.html--}}
-@if ($message = Session::get('error'))
-    <div class="alert alert-danger alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
-    </div>
-@endif
-
-
