558ab957c20d4f72475e05ffa99483a89c856423
Taehong <minth1123@icloud.com>
2022-07-14 10:22:29 -0300

    Adding a posted by functionality

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/taehong/Downloads/PHP/laravel/final/QutesApp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/taehong/Downloads/PHP/laravel/final/QutesApp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/taehong/Downloads/PHP/laravel/final/QuotesApp/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/taehong/Downloads/PHP/laravel/final/QuotesApp/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
